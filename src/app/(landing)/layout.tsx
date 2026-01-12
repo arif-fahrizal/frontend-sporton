ae11d7bcf80d734f5288fd3fb432ed4d47158854
arif-fahrizal <fahrizalarif07@gmail.com>
2026-01-12 22:27:36 +0700

    Add assets & Style Configuration

---
+++ src/app/(landing)/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next';
+import { Poppins } from 'next/font/google';
+import '../globals.css';
+
+const poppins = Poppins({
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['400', '500', '600', '700', '800'],
+});
+
+export const metadata: Metadata = {
+  title: 'Sport On',
+  description:
+    'Engineered for endurance and designed for speed. Experience gear that moves as fast as you do. Premium fabrics. Unmatched comfort. Limitless motion.',
+};
+
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
+  return (
+    <html lang="en">
+      <body className={`${poppins.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
