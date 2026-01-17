8633b26857cb99524154758be81a2fcf64142eb4
arif-fahrizal <fahrizalarif07@gmail.com>
2026-01-18 03:53:04 +0700

    [FEAT] CheckoutsPage: Change the layout system in the mobile version from flexbox to grid

--- src/app/(landing)/checkouts/page.tsx
+++ src/app/(landing)/checkouts/page.tsx
@@ -6,7 +6,7 @@ export default function CheckoutsPage() {
     <main className="min-h-[80vh] px-4 bg-gray-100">
       <div className="max-w-5xl mx-auto py-10 lg:py-20">
         <h1 className="mb-10 text-4xl text-center font-bold sm:text-5xl">Checkout Now</h1>
-        <div className="flex flex-col-reverse gap-5 sm:grid sm:grid-cols-2 sm:gap-10 lg:gap-14">
+        <div className="grid gap-5 sm:grid-cols-2 sm:gap-10 lg:gap-14">
           <OrderInformation />
           <CartItems />
         </div>
