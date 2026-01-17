30fd5f41c337050e2513e868b31965549b7f26a3
arif-fahrizal <fahrizalarif07@gmail.com>
2026-01-18 03:29:31 +0700

    [FEAT] PaymentsPage: Implementation responsive layout

--- src/app/(landing)/payments/page.tsx
+++ src/app/(landing)/payments/page.tsx
@@ -3,10 +3,10 @@ import PaymentSteps from '@/app/(landing)/_components/Pages/Payments/PaymentStep
 
 export default function PaymentsPage() {
   return (
-    <main className="min-h-[80vh] bg-gray-100">
-      <div className="max-w-5xl mx-auto py-20">
-        <h1 className="mb-11 text-5xl text-center font-bold">Payments</h1>
-        <div className="grid grid-cols-2 gap-14">
+    <main className="min-h-[80vh] px-4 bg-gray-100">
+      <div className="max-w-5xl mx-auto py-10 lg:py-20">
+        <h1 className="mb-10 text-4xl text-center font-bold sm:text-5xl">Payments</h1>
+        <div className="grid gap-5 sm:grid-cols-2 sm:gap-10 lg:gap-14">
           <PaymentOptions />
           <PaymentSteps />
         </div>
