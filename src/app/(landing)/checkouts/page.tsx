389a8aa05fae01918a7b0352f5196d73de227d28
arif-fahrizal <fahrizalarif07@gmail.com>
2026-01-18 03:25:44 +0700

    [FEAT] CheckoutsPage: Implementation responsive layout

--- src/app/(landing)/checkouts/page.tsx
+++ src/app/(landing)/checkouts/page.tsx
@@ -3,14 +3,14 @@ import OrderInformation from '@/app/(landing)/_components/Pages/Checkout/OrderIn
 
 export default function CheckoutsPage() {
   return (
-    <div className="min-h-[80vh] bg-gray-100">
-      <div className="max-w-5xl mx-auto py-20">
-        <h1 className="mb-11 text-5xl text-center font-bold">Checkout Now</h1>
-        <div className="grid grid-cols-2 gap-14">
+    <main className="min-h-[80vh] px-4 bg-gray-100">
+      <div className="max-w-5xl mx-auto py-10 lg:py-20">
+        <h1 className="mb-10 text-4xl text-center font-bold sm:text-5xl">Checkout Now</h1>
+        <div className="flex flex-col-reverse gap-5 sm:grid sm:grid-cols-2 sm:gap-10 lg:gap-14">
           <OrderInformation />
           <CartItems />
         </div>
       </div>
-    </div>
+    </main>
   );
 }
